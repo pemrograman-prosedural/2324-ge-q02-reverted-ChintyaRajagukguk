5796e32dc234f5ea8dcf682d97e0ad13e3d2cfb9
ChintyaRajagukguk <143678980+ChintyaRajagukguk@users.noreply.github.com>
2024-05-12 00:19:38 +0700

    Update q02_01.c

--- q02_01.c
+++ q02_01.c
@@ -1,6 +1,3 @@
-//12S23030 - SimorangkirJonathan
-//12S23045 - Chintya ReginaUli Rajagukguk
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,68 +5,95 @@
 #include "./libs/student.h"
 #include "./libs/gender.h"
 
+int main(int argc, char **argv)
+{
+    char input[75];
+    int zdrm = 0;
+    int size = 0;
+    struct dorm_t *drm = malloc(size * sizeof(struct dorm_t));
+    int zstd = 0;
+    int sizee = 0;
+    struct student_t *mhs = malloc(sizee * sizeof(struct student_t));
+    char *token;
 
-token = strtok(input, "#");
-if (strcmp(token, "---") == 0)
-{
-  break;
-}
-else if (strcmp(token, "dorm-add") == 0)
-{
-  size++;
-  drm = realloc(drm, size * sizeof(struct dorm_t));
-  drm[zdrm] = create_dorm(input);
-  zdrm++;
-}
-else if (strcmp(token, "dorm-print-all") == 0)
-{
-  for (int m = 0; m < zdrm; m++)
-  {
-    print_dorm(drm[m]);
-  }
-}
-else if (strcmp(token, "dorm-print-all-detail") == 0)
-{
-  for (int m = 0; m < zdrm; m++)
-  {
-    print_dorm_detail(drm[m]);
-  }
-}
-else if (strcmp(token, "student-add") == 0)
-{
-  sizee++;
-  mhs = realloc(mhs, sizee * sizeof(struct student_t));
-  mhs[zstd] = create_student(input);
-  zstd++;
-}
-else if (strcmp(token, "student-print-all") == 0)
-{
-  for (int m = 0; m < zstd; m++)
-  {
-    print_student(mhs[m]);
-  }
-}
-else if (strcmp(token, "student-print-all-detail") == 0)
-{
-  for (int m = 0; m < zstd; m++)
-  {
-    print_student_detail(mhs[m]);
-  }
-}
-else if (strcmp(token, "assign-student") == 0)
-{
-  char *nim = strtok(NULL, "#");
-  char *asrama = strtok(NULL, "#");
-  assign_student(drm, mhs, nim, asrama, zstd, zdrm, find_id, find_dorm);
-}
-else if (strcmp(token, "move-student") == 0)
-{
-  char *nim = strtok(NULL, "#");
-  char *asrama = strtok(NULL, "#");
-  move_student(drm, mhs, nim, asrama, zstd, zdrm, find_id, find_dorm);
-}
-else if (strcmp(token, "dorm-empty") == 0)
-{
-  char *asrama = strtok(NULL, "#");
-  dorm_empty(asrama, zstd, zdrm, mhs, drm, find_dorm);
+    // Use a boolean flag to exit the loop
+    bool running = true;
+    while (running)
+    {
+        fgets(input, sizeof input, stdin);
+
+        // Remove trailing newline or carriage return
+        input[strcspn(input, "\r\n")] = '\0';
+
+        token = strtok(input, "#");
+
+        if (strcmp(token, "---") == 0)
+        {
+            running = false; // Exit the loop
+        }
+        else if (strcmp(token, "dorm-add") == 0)
+        {
+            size++;
+            drm = realloc(drm, size * sizeof(struct dorm_t));
+            drm[zdrm] = create_dorm(input);
+            zdrm++;
+        }
+        else if (strcmp(token, "dorm-print-all") == 0)
+        {
+            for (int m = 0; m < zdrm; m++)
+            {
+                print_dorm(drm[m]);
+            }
+        }
+        else if (strcmp(token, "dorm-print-all-detail") == 0)
+        {
+            for (int m = 0; m < zdrm; m++)
+            {
+                print_dorm_detail(drm[m]);
+            }
+        }
+        else if (strcmp(token, "student-add") == 0)
+        {
+            sizee++;
+            mhs = realloc(mhs, sizee * sizeof(struct student_t));
+            mhs[zstd] = create_student(input);
+            zstd++;
+        }
+        else if (strcmp(token, "student-print-all") == 0)
+        {
+            for (int m = 0; m < zstd; m++)
+            {
+                print_student(mhs[m]);
+            }
+        }
+        else if (strcmp(token, "student-print-all-detail") == 0)
+        {
+            for (int m = 0; m < zstd; m++)
+            {
+                print_student_detail(mhs[m]);
+            }
+        }
+        else if (strcmp(token, "assign-student") == 0)
+        {
+            char *nim = strtok(NULL, "#");
+            char *asrama = strtok(NULL, "#");
+            assign_student(drm, mhs, nim, asrama, zstd, zdrm, find_id, find_dorm);
+        }
+        else if (strcmp(token, "move-student") == 0)
+        {
+            char *nim = strtok(NULL, "#");
+            char *asrama = strtok(NULL, "#");
+            move_student(drm, mhs, nim, asrama, zstd, zdrm, find_id, find_dorm);
+        }
+        else if (strcmp(token, "dorm-empty") == 0)
+        {
+            char *asrama = strtok(NULL, "#");
+            dorm_empty(asrama, zstd, zdrm, mhs, drm, find_dorm);
+        }
+    }
+
+    free(mhs);
+    free(drm);
+
+    return 0;
 }
